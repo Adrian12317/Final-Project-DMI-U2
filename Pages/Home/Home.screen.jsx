33dc22387b4f6feec2644f09d4703ce0688bdfde
Edson <edsonotz98@gmail.com>
2021-11-14 14:41:14 -0600

    Home page

---
+++ Pages/Home/Home.screen.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import TodoList from '../TodoList';
+import Profile from '../Profile';
+import LogOut from '../Logout';
+
+
+
+const Tab = createBottomTabNavigator();
+
+export default function HomePage() {
+  const [photos,setPhotos] = useState([])
+
+  return (
+    <>
+      <Tab.Navigator screenOptions={({route})=>({
+        tabBarIcon:({focused,color,size})=>{
+          let iconName;
+         if (route.name === "TodoList") {
+            iconName = focused ?
+            "ios-images"
+            :
+            "ios-images-outline";
+          }else if (route.name === "Profile") {
+            iconName = focused ?
+            "ios-person-sharp"
+            :
+            "ios-person-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color}/>;
+        },
+        tabBarActiveTintColor: "green",
+        tabBarInactiveTintColor :"grey",
+      })}
+      >
+       
+       
+        <Tab.Screen name="TodoList" 
+         options={{
+          headerRight: () => (
+            <LogOut  />
+          ),
+        }}
+        >
+        {(props) => <TodoList{...props} />}</Tab.Screen>
+
+        
+        <Tab.Screen name="Profile" 
+         options={{
+          headerRight: () => (
+            <LogOut  />
+          ),
+        }}
+        >
+        {(props) => <Profile{...props} />}</Tab.Screen>
+        
+      </Tab.Navigator>
+    </>
+  );
+}
